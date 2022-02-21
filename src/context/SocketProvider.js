fa52031883f53e8c53a719d31e4abdfad89e7f89
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-02-21 14:07:36 +0300

    Edited SOCKET_URL

--- src/context/SocketProvider.js
+++ src/context/SocketProvider.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import SocketContext from "./socket-context";
 
 const SocketProvider = (props) => {
-  const SOCKET_URL = `${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+  const SOCKET_URL = `${process.env.REACT_APP_SOCKET_URL}`;
   const socket = io.connect(SOCKET_URL);
   return (
     <SocketContext.Provider value={socket}>
